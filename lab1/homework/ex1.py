99649794f843f4953a2a47974e8d3afb7b765aa8
hoangcuong9x <spy12a6@gmail.com>
2018-11-04 17:30:58 +0700

    initial commit

--- lab1/homework/ex1.py
+++ lab1/homework/ex1.py
@@ -3,10 +3,9 @@ from random import choice
 import datetime
 gmail = GMail("spy12a6@gmail.com","cuong11a6")
 message = Message("Don xin nghi", to="cuongnh1305.namtien@gmail.com", text="em bi dau bung")
-now = datetime.datetime.now()
-n = now.hour, now.minute
-timer = "07:00"
+now = datetime.datetime.now().strftime("%H:%M %p")
+
 while True:
-    if timer !=n:
+    now = datetime.datetime.now().strftime("%I:%M %p")
+    if now == "07:00 AM":
         gmail.send(message)
-        break
